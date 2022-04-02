626793b9ef823a734897bb2e4d7ecb7a03fd91ff
Assurance Femi <assurance.femi@raenest.com>
2022-04-02 10:43:49 +0100

    Fixed link issue

--- src/components/TVShowsBox.js
+++ src/components/TVShowsBox.js
@@ -24,51 +24,52 @@ const TVShowsBox = ({ tvShows, notFoundText, showCtxtMenu })=> {
                     <div className="flex justify-center items-center m-8">{notFoundText}</div> :
 
                     tvShows?.map((x, i) => (
-                        <Link key={x.id} to={`/details/tv/${x.id}`}>
-                            <div  className="bg-[#20283E] h-[35em] rounded-lg p-4 flex[1_1_21%] box-border">
+                        <div key={x.id}  className="bg-[#20283E] h-[35em] rounded-lg p-4 flex[1_1_21%] box-border">
+                            <Link to={`/details/tv/${x.id}`}>
                                 <div className="h-5/6 rounded-lg">
                                     <img className="rounded-lg" src={`https://image.tmdb.org/t/p/w500${x.poster_path}`} alt={x.name}  />
                                 </div>
+                            </Link>
 
-                                <div className="p-2 relative flex flex-col justify-center">
-                                    <span>{x.name}</span>
-                                    <div className="flex justify-between items-end mb-5">
-                                        <div className="font-extrabold text-[#7B6EF6]">{x.vote_average}</div>
-                                        {
-                                            showCtxtMenu &&
-                                            <div 
-                                                onClick={()=> {
-                                                    if(menuCtxtBox.current[i].style.display === 'none'){
-                                                        menuCtxtBox.current[i].style.display = 'flex'
-                                                    } else {
-                                                        menuCtxtBox.current[i].style.display = 'none';
-                                                    }
-                                                }}
-                                                className="font-extrabold text-4xl cursor-pointer"
-                                            >
-                                                ...
-                                            </div>
-                                        }
-                                    </div>
 
+                            <div className="p-2 relative flex flex-col justify-center">
+                                <span>{x.name}</span>
+                                <div className="flex justify-between items-end mb-5">
+                                    <div className="font-extrabold text-[#7B6EF6]">{x.vote_average}</div>
                                     {
-                                        
+                                        showCtxtMenu &&
                                         <div 
-                                            ref={el => { menuCtxtBox.current[i] = el; menuCtxtBox.current[i] ? menuCtxtBox.current[i].style.display = "none" : menuCtxtBox.current[i] = null }}
-                                            className="absolute right-0 bottom-12 bg-white text-black p-4 flex flex-col font-extrabold rounded-lg gap-y-4 w-[10em] text-sm"
+                                            onClick={()=> {
+                                                if(menuCtxtBox.current[i].style.display === 'none'){
+                                                    menuCtxtBox.current[i].style.display = 'flex'
+                                                } else {
+                                                    menuCtxtBox.current[i].style.display = 'none';
+                                                }
+                                            }}
+                                            className="font-extrabold text-4xl cursor-pointer"
                                         >
-                                            <div 
-                                                onClick={()=> { handleMarkViewedTVShows(x.id); menuCtxtBox.current[i].style.display = 'none'}}
-                                                className="cursor-pointer hover:text-[#7B6EF6]"
-                                            >
-                                                Mark Viewed
-                                            </div>
-                                            <div className="cursor-pointer hover:text-[#7B6EF6]">Add Notes</div>
+                                            ...
                                         </div>
                                     }
                                 </div>
+
+                                {
+                                    
+                                    <div 
+                                        ref={el => { menuCtxtBox.current[i] = el; menuCtxtBox.current[i] ? menuCtxtBox.current[i].style.display = "none" : menuCtxtBox.current[i] = null }}
+                                        className="absolute right-0 bottom-12 bg-white text-black p-4 flex flex-col font-extrabold rounded-lg gap-y-4 w-[10em] text-sm"
+                                    >
+                                        <div 
+                                            onClick={()=> { handleMarkViewedTVShows(x.id); menuCtxtBox.current[i].style.display = 'none'}}
+                                            className="cursor-pointer hover:text-[#7B6EF6]"
+                                        >
+                                            Mark Viewed
+                                        </div>
+                                        <div className="cursor-pointer hover:text-[#7B6EF6]">Add Notes</div>
+                                    </div>
+                                }
                             </div>
-                        </Link>
+                        </div>
                     ))
                 }
             </div>
